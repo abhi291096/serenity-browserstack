0771ad42386871e178ff6650322b545628cf1415
abhi291096 <43001934+abhi291096@users.noreply.github.com>
2018-11-12 23:09:35 +0530

    Add  caps

--- src/test/java/com/browserstack/BrowserStackSerenityDriver.java
+++ src/test/java/com/browserstack/BrowserStackSerenityDriver.java
@@ -42,6 +42,7 @@ public class BrowserStackSerenityDriver implements DriverSource {
                 capabilities.setCapability(key.replace("bstack_", ""), environmentVariables.getProperty(key));
                 if (key.equals("bstack_browserstack.local") && environmentVariables.getProperty(key).equalsIgnoreCase("true")) {
                     System.setProperty("browserstack.local", "true");
+                    capabilities.setCapability("browserstack.localIdentifier","randomstring123456");
                 }
             } else if (environment != null && key.startsWith("environment." + environment)) {
                 capabilities.setCapability(key.replace("environment." + environment + ".", ""), environmentVariables.getProperty(key));
